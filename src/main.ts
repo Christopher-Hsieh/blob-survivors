4aa1388259d42fdb899a064c96547ec40d1d1d12
Chris <chrishsieh@live.com>
2023-03-24 21:55:14 -0400

    Setup first scene.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,45 @@
 import Phaser from 'phaser';
-import config from './config';
-import GameScene from './scenes/Game';
+import { menu } from './scenes/menu-scene';
 
-new Phaser.Game(
-  Object.assign(config, {
-    scene: [GameScene]
-  })
-);
+const GameConfig: Phaser.Types.Core.GameConfig = {
+  title: 'ExampleGame',
+  version: '1.0',
+  width: 800,
+  height: 600,
+  type: Phaser.AUTO,
+  parent: 'app',
+  // `as as Phaser.Types.Scenes.SettingsConfig[]` is required until https://github.com/photonstorm/phaser/pull/6235
+  scene: [menu()] as Phaser.Types.Scenes.SettingsConfig[],
+  input: {
+    keyboard: true
+  },
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+  backgroundColor: '#300000',
+  render: { pixelArt: false, antialias: true },
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    // `fullscreenTarget` must be defined for phones to not have
+    // a small margin during fullscreen.
+    fullscreenTarget: 'app',
+    expandParent: false,
+  },
+};
+
+
+export class Game extends Phaser.Game {
+  constructor(config: Phaser.Types.Core.GameConfig) {
+    super(config);
+  }
+}
+
+window.addEventListener('load', () => {
+  // Expose `_game` to allow debugging, mute button and fullscreen button
+  (window as any)._game = new Game(GameConfig);
+});
