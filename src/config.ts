dbdf6eec476e06ce3947870ed37731b3fbae3efb
Chris <chrishsieh@live.com>
2023-03-24 20:56:07 -0400

    Initial setup: vite, ts, etc.

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ import Phaser from 'phaser';
 export default {
   type: Phaser.AUTO,
   parent: 'game',
-  backgroundColor: '#33A5E7',
+  backgroundColor: '#B2C7C8',
   scale: {
     width: 800,
     height: 600,
